93e74f1c459813fd0425b3fa6407f1f0bfb877f6
maban <mateusz@banasiewicz.net>
2023-04-23 16:09:55 +0200

    fixes

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 module "k8s" {
-  source = "git@github.com:maban92/terrform-module-libvirt.git"
+  source = "../"
 
   network_cidr    = "10.12.12.0/24"
   project_name    = "k8s"
@@ -9,18 +9,18 @@ module "k8s" {
   vms = {
     master = {
       cpu    = 4,
-      memory = 2048,
-      hostID = 10 
+      memory = 10240,
+      IPaddresses = ["10.12.12.100"]
     }
     node01 = { 
       cpu    = 4,
-      memory = 4096,
-      hostID = 11
+      memory = 10240,
+      IPaddresses = ["10.12.12.101"]
     }
     node02 = {
       cpu    = 4,
-      memory = 4096,
-      hostID = 12
+      memory = 10240,
+      IPaddresses = ["10.12.12.102"]
     }
   }
 }
--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "libvirt" {
 }
 
 locals {
-  network_name        = format("tf-%s_%s", var.project_name, replace(replace(var.network_cidr, ".", "-"), "/","--"))
-  network_bridge      = format("tf-%s", var.project_name)
+  network_name        = format("tf--%s_%s", var.project_name, replace(replace(var.network_cidr, ".", "-"), "/","--"))
+  network_bridge      = format("tf--%s", var.project_name)
   network_domain      = format("%s.%s", var.project_name, var.domain)
-  pool_name           = format("tf-%s", var.project_name)
+  pool_name           = format("tf--%s", var.project_name)
   pool_path           = format("/var/lib/libvirt/images/%s", var.project_name)
   volume_source_path  = format("/var/lib/libvirt/template/%s", var.template)
 }
@@ -50,14 +50,14 @@ resource "libvirt_pool" "pool" {
 
 resource "libvirt_cloudinit_disk" "cloudinit_disk" {
   for_each       = var.vms
-  name           = format("tf-%s_%s-seed.iso", var.project_name, each.key)
+  name           = format("tf--%s_%s-seed.iso", var.project_name, each.key)
   user_data      = data.template_file.cloudinit_cfg_file[each.key].rendered
   pool           = libvirt_pool.pool.name
 }
 
 resource "libvirt_volume" "volume" {
   for_each   = var.vms
-  name       = format("tf-%s_%s-boot.gcow2", var.project_name, each.key)
+  name       = format("tf--%s_%s-boot.gcow2", var.project_name, each.key)
   pool       = libvirt_pool.pool.name
   source     = local.volume_source_path
   format     = "qcow2"
@@ -65,7 +65,7 @@ resource "libvirt_volume" "volume" {
 
 resource "libvirt_domain" "domain_master" {
   for_each  = var.vms
-  name      = format("tf-%s_%s_%s", var.project_name, each.key, replace(cidrhost(var.network_cidr, each.value.hostID), ".", "-"))
+  name      = format("tf--%s_%s_ip_%s", var.project_name, each.key, replace(each.value.IPaddresses[0], ".", "-"))
   memory    = each.value.memory
   vcpu      = each.value.cpu
 
@@ -73,7 +73,7 @@ resource "libvirt_domain" "domain_master" {
 
   network_interface {
     network_id     = libvirt_network.network.id
-    addresses      = [cidrhost(var.network_cidr, each.value.hostID)]
+    addresses      = each.value.IPaddresses
     hostname       = format("%s.%s.%s", each.key, var.project_name, var.domain)
   }
 
